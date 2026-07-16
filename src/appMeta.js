b8d76a6a70d40b8943025eeff03dd1674497df60
Marouane Al Mandri <marouane.almandri@gmail.com>
2026-07-16 13:43:19 +0200

    Release version 0.2.1

--- src/appMeta.js
+++ src/appMeta.js
@@ -1,3 +1,2 @@
-export const APP_VERSION = '0.2.0';
-export const APP_LAST_UPDATED = '2026-07-16T12:59:34+02:00';
-
+export const APP_VERSION = '0.2.1';
+export const APP_LAST_UPDATED = '2026-07-16T13:40:31+02:00';
